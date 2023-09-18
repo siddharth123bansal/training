c3dd496ca46606bc546dc9270db67f406ca55355
siddharth <chinubansal766@gmail.com>
2023-09-19 00:01:36 +0530

    Movies

--- src/pages/MoviesList.jsx
+++ src/pages/MoviesList.jsx
@@ -6,7 +6,7 @@ function formatDateToDdMmYyyy(dateString) {
   const date = new Date(dateString);
 
   const day = String(date.getDate())
-  const month = String(date.getMonth() + 1) // Month is zero-based, so we add 1
+  const month = String(date.getMonth() + 1)
   const year = date.getFullYear();
 
   return `${day} - ${month} - ${year}`;
