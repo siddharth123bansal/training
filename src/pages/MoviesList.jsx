ca1bf8081a68f8e673495f94768219d26d9dc4e1
siddharth <chinubansal766@gmail.com>
2023-09-19 00:04:48 +0530

    movies

--- src/pages/MoviesList.jsx
+++ src/pages/MoviesList.jsx
@@ -1,20 +1,15 @@
-// src/pages/MovieList.jsx
 import React from 'react';
 import MovieCard from '@/components/MoviesCard';
 import Link from 'next/link';
 function formatDateToDdMmYyyy(dateString) {
   const date = new Date(dateString);
-
   const day = String(date.getDate())
   const month = String(date.getMonth() + 1)
   const year = date.getFullYear();
-
   return `${day} - ${month} - ${year}`;
 }
 const MovieList = ({ dataarray }) => {
-  const url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_zb4Tljpe8TLrYAuPGBiWXWkkpvV7dGjzGA&usqp=CAU';
-
-  return (
+   return (
     <div className="movie-list" style={{ display: 'flex',marginLeft:'20px', flexWrap: 'wrap' }}>
       {dataarray.map((movie) => (
         <Link href={`/addmovies/${movie.id}`} key={movie.id}>
@@ -24,5 +19,4 @@ const MovieList = ({ dataarray }) => {
     </div>
   );
 };
-
 export default MovieList;
