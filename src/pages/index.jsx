9489d0e96021b24cc8f6dd88a1dbc3b359ec5aa2
siddharth <chinubansal766@gmail.com>
2023-09-18 23:32:05 +0530

    engine added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,31 +6,31 @@ const axios = require('axios');
 function IndexPage() {
   const [movieData, setMovieData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-      axios.get('https://movies-7hu0.onrender.com/movies')
-        .then((response) => {
-          setMovieData(response.data);
-          setTimeout(() => {
-            setIsLoading(false);
-          }, 2000);
-        })
-        .catch((error) => {
-          console.error('Error fetching movie data:', error);
-        });
+
+  useEffect(() => {
+    axios.get('https://movies-7hu0.onrender.com/movies')
+      .then((response) => {
+        console.log('Received movie data:', response.data);
+        setMovieData(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching movie data:', error);
       });
-  
+  }, []);
 
   return (
     <>
-    <Headers/>
-    {isLoading?(<Loading/>):(
-    <div>
-      <h1 className='text-7xl text-red-500 m-10'>Movies</h1>
-      <MovieList dataarray={movieData} />
-      </div>
-    )}
+      <Headers />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          <h1 className='text-7xl text-red-500 m-10'>Movies</h1>
+          <MovieList dataarray={movieData} />
+        </div>
+      )}
     </>
   );
 }
-
 export default IndexPage;
