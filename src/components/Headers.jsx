fd66dfe344ebfddd37321e0d0642757a1fcdffec
siddharth <chinubansal766@gmail.com>
2023-09-19 11:28:48 +0530

    header

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -4,15 +4,10 @@ import React from "react";
 const Headers = () => {
   return (
     <div>
-      <div style={{ display: "flex" }}>
-        <p
-          className="text-red-600 text-5xl mx-10"
-          // style={{ margin: "20px", fontSize: "20px" }} 
-        >
-          Movies
-        </p>
-        <nav style={{ marginTop: "28px", fontSize: "12px" }}>
-          <ul className="flex space-x-4 text-white">
+      <div className="flex flex-col md:flex-row items-center">
+        <p className="text-red-600 text-5xl mx-10">Movies</p>
+        <nav className="md:mt-0 mt-4">
+          <ul className="flex flex-col md:flex-row space-x-4 md:space-x-8 md:text-base text-sm text-white">
             <li>
               <Link href="/">Home</Link>
             </li>
@@ -26,8 +21,8 @@ const Headers = () => {
               <Link href="/directors/adddirector">Add Directors</Link>
             </li>
             <li>
-            <Link href="/assigndirectors">Assign Directors</Link>
-          </li>
+              <Link href="/assigndirectors">Assign Directors</Link>
+            </li>
           </ul>
         </nav>
       </div>
